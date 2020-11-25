25d14fb1fc7f19dc5ff556d0a86b90841dccd1ff
jeyxeon <jeyxxs@gmail.com>
2020-11-25 19:18:48 +0300

    seventh_task

--- src/tasks/Task7.java
+++ src/tasks/Task7.java
@@ -7,6 +7,7 @@ import common.Vacancy;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Из коллекции компаний необходимо получить всевозможные различные названия вакансий
@@ -14,7 +15,11 @@ import java.util.Set;
 public class Task7 implements Task {
 
   private Set<String> vacancyNames(Collection<Company> companies) {
-    return new HashSet<>();
+    // Для каждой компании обращаюсь к сету Vacancies,
+    // получаю название и вывожу уникальные значения в сет
+    // (distinct излишний, так при формировании set-а добавляются уникальные значения).
+    return companies.stream().flatMap(company -> company.getVacancies().stream()
+      .map(Vacancy::getTitle)).collect(Collectors.toSet());
   }
 
   @Override
